0b63329d668cf86d805b42f11b4332e0f6533b16
SAFAKARSLI <safakarsli.com@gmail.com>
2023-11-25 11:18:49 -0500

    Initialize react-app :

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import { read, utils, writeFile } from 'xlsx';
+import { useState } from 'react';
+
 
 function App() {
+
+  const [pres, setPres] = useState([]);
+  console.log(file)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>Hello world</h1>
     </div>
   );
 }
